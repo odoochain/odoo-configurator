a80de82313d0efbd1d7c9c995ca5a4060665d41a
Michel Perrocheau <michel@scalizer.fr>
2024-09-24 10:12:13 +0200

    [IMP] improve logging

--- src/odoo_configurator/apps/modules.py
+++ src/odoo_configurator/apps/modules.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Copyright (C) 2023 - Teclib'ERP (<https://www.teclib-erp.com>).
+# Copyright 2024 Scalizer (<https://www.scalizer.fr>)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 import time
@@ -24,41 +25,38 @@ class OdooModules(base.OdooModule):
             self.update_modules(self._pre_datas)
             self.uninstall_modules(self._pre_datas)
 
-    def install_config_modules(self):
-        self.install_modules(self._datas)
-        self.update_modules(self._datas)
-        self.uninstall_modules(self._datas)
+    def install_config_modules(self, datas=None):
+        if not datas:
+            datas = self._datas
+        self.install_modules(datas)
+        self.update_modules(datas)
+        self.uninstall_modules(datas)
 
     def install_modules(self, config):
-        self.logger.info("Install modules")
         self.install_odoo(config.get('modules', []))
         for key in config:
             if isinstance(config.get(key), dict) or isinstance(config.get(key), OrderedDict):
                 modules = config.get(key).get('modules', [])
                 if modules:
-                    self.logger.info("\t- Modules for %s" % key)
+                    self.logger.info("Install Modules for %s" % key)
                     self.install_odoo(modules)
 
     def update_modules(self, config):
-        self.logger.info("Update modules")
-
         self.update_module_odoo(config.get('updates', []))
         for key in config:
             if isinstance(config.get(key), dict) or isinstance(config.get(key), OrderedDict):
                 modules = config.get(key).get('updates', [])
                 if modules:
-                    self.logger.info("\t- Modules for %s" % key)
+                    self.logger.info("Update Modules for %s" % key)
                     self.update_module_odoo(modules)
 
     def uninstall_modules(self, config):
-        self.logger.info("Uninstall modules")
-
         self.uninstall_odoo(config.get('uninstall_modules', []))
         for key in config:
             if isinstance(config.get(key), dict) or isinstance(config.get(key), OrderedDict):
                 modules = config.get(key).get('uninstall_modules', [])
                 if modules:
-                    self.logger.info("\t- Uninstall Modules for %s" % key)
+                    self.logger.info("Uninstall Modules for %s" % key)
                     self.uninstall_odoo(modules)
 
     def update_list(self):
@@ -78,7 +76,7 @@ class OdooModules(base.OdooModule):
             to_install = []
             missing_modules = []
             for module in modules:
-                self.logger.info('\t\t* %s' % module)
+                self.logger.info('\t* %s' % module)
                 if self._modules_cache.get(module):
                     if self._modules_cache.get(module).get('state') != 'installed':
                         to_install.append(self._modules_cache.get(module).get('id'))
@@ -86,14 +84,14 @@ class OdooModules(base.OdooModule):
                     missing_modules.append(module)
 
             if missing_modules:
-                self.logger.error("\t\tModules not found : %s " % (", ".join(missing_modules)))
+                self.logger.error("\tModules not found : %s " % (", ".join(missing_modules)))
 
             if to_install:
                 self.execute_odoo('ir.module.module', 'button_immediate_install', [to_install])
 
     def update_module_odoo(self, modules):
         if modules:
-            self.logger.info('\t\tUpdate %s', modules)
+            self.logger.info('\tUpdate %s', modules)
             self.execute_odoo('ir.module.module', 'button_immediate_upgrade',
                               [[self._connection.get_id_from_xml_id("base.module_" + m) for m in modules]])
 
@@ -104,7 +102,7 @@ class OdooModules(base.OdooModule):
         if modules:
             to_uninstall = []
             for module in modules:
-                self.logger.info('\t\t* %s' % module)
+                self.logger.info('\t* %s' % module)
                 if self._uninstalled_modules_cache.get(module).get('state') != 'uninstalled':
                     to_uninstall.append(self._modules_cache.get(module).get('id'))
 
